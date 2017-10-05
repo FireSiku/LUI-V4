3e4dea192df238ddb7c228a19c69889741ae67d2
FireSiku <chaoslux@gmail.com>
2017-10-05 02:08:41 -0400

    Rev: Bit weird calling it Revision on Git.

--- api/curse.lua
+++ api/curse.lua
@@ -1,4 +1,4 @@
 -- This file is mainly used to hold Curse repository replacements. Do Not Touch.
 -- Do not ever commit this file, as local versions will most likely replaced with static data.
 local addonname, LUI = ...
-LUI.revision = "@project-revision@"
+LUI.curseVersion = "@project-version@"
